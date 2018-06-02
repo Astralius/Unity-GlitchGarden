7765b7dd48586647ddaf23b707e7251e816e8b54
Adrian Standowicz <adrian.standowicz@gmail.com>
2018-06-02 18:09:10 +0200

    Initial scenes layout
    Added initial scenes layout - splash screen and start menu scene. Modified LevelManager accordingly.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public float autoLoadDelay;
+
+    private void Start() 
+    {
+        Invoke("LoadNextLevel", autoLoadDelay);
+    }
+
+    public void LoadLevel(string levelName)
+    {
+        Cursor.visible = true;
+        SceneManager.LoadScene(levelName, LoadSceneMode.Single);        
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
