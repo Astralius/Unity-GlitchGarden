a26940985f207e46a92bb7b2d71bbb0324079e61
Adrian Standowicz <adrian.standowicz@gmail.com>
2018-06-25 22:51:37 +0200

    Projectiles
    Added first two projectiles to the game: cactus spikes & swirling axe, along with generic Mover and Rotator behaviours.

---
+++ Assets/Mover.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [Tooltip("Linear velocity in Unity units per second.")]
+    public Vector3 MovementSpeed;
+
+    void Update()
+    {
+        transform.position += MovementSpeed * Time.deltaTime;
+    }
+}
---
+++ Assets/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [Tooltip("Angular velocity in degrees per second.")]
+    public Vector3 RotationSpeed;
+
+    void Update()
+    {
+        transform.Rotate(RotationSpeed * Time.deltaTime);
+    }
+}
