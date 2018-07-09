1c2233acfc95559ce30992ba6f3d252ac7b56434
Adrian Standowicz <adrian.standowicz@gmail.com>
2018-07-09 23:03:02 +0200

    Construction pane (UI only)
    Added construction pane which allows to toggle-pick a defender to be spawned in the scene.

---
+++ Assets/Scripts/UI/ConstructionItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ConstructionItem : MonoBehaviour, IPointerDownHandler
+{
+    public Defender DefenderPrefab;  
+
+    [SerializeField] 
+#pragma warning disable 649
+    private Image defenderImage;
+#pragma warning restore 649
+
+    public DefenderSelectedEvent DefenderSelected;
+
+    public void Enable()
+    {
+        defenderImage.color = Color.white;
+    }
+
+    public void Disable()
+    {
+        defenderImage.color = Color.black;
+    }
+
+    [Serializable]
+    public class DefenderSelectedEvent : UnityEvent<ConstructionItem> {}
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        DefenderSelected.Invoke(this);       
+    }
+}
---
+++ Assets/Scripts/UI/ConstructionPane.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstructionPane : MonoBehaviour
+{
+    private readonly List<ConstructionItem> availableDefenders = new List<ConstructionItem>();
+    private Defender selectedDefender;
+
+    public GameObject SelectedDefenderPrefab
+    {
+        get
+        {
+            GameObject result = null;
+            if (selectedDefender)
+            {
+                result = selectedDefender.gameObject;
+            }
+            return result;
+        }
+        set
+        {
+            var defender = value.GetComponent<Defender>();
+            if (defender)
+            {
+                selectedDefender = defender;
+            }
+            else
+            {
+                throw new ArgumentException("Only Defenders allowed! (i.e. GameObjects with Defender component)");
+            }
+        }
+    }
+
+    public void UpdateSelection(ConstructionItem selectedItem)
+    {
+        if (!availableDefenders.Contains(selectedItem))
+        {
+            availableDefenders.Add(selectedItem);
+        }
+
+        availableDefenders.ForEach(defender => defender.Disable());
+
+        if (selectedItem != null)
+        {
+            if (selectedDefender == selectedItem.DefenderPrefab)
+            {
+                selectedItem.Disable();
+                selectedDefender = null;
+            }
+            else
+            {
+                selectedItem.Enable();
+                selectedDefender = selectedItem.DefenderPrefab;
+            }          
+        }
+        else
+        {
+            Debug.LogWarning("Calling UpdateSelection with null has no effect!");
+        }
+    }
+}
