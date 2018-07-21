df0fd7b5c39c72ab6d0cd972a782b42c55076955
Adrian Standowicz <adrian.standowicz@gmail.com>
2018-07-21 23:58:39 +0200

    Lose Collider
    Added lose collider prefab and placed it in the first level to make it possible to lose the game.

---
+++ Assets/Scripts/Global/LoseCollider.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class LoseCollider : MonoBehaviour
+{
+    private LevelManager levelManager;
+
+    void Start()
+    {
+        levelManager = FindObjectOfType<LevelManager>();
+        if (levelManager == null)
+        {
+            Debug.LogError("Could not find a Level Manager in the scene!");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        levelManager.LoadLevel("03b Lose");
+    }
+}
